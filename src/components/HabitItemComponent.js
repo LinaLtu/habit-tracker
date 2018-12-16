c6baf70fbb8abd3e98e81f1974b7b9a89ed89179
LinaLtu <lsuodyte@babbel.com>
2018-12-16 18:18:18 +0100

    feat: added icons to habitItems

--- src/components/HabitItemComponent.js
+++ src/components/HabitItemComponent.js
@@ -1,32 +1,49 @@
 import moment from 'moment';
 import React, { Component } from 'react';
+import MaterialIcon from 'material-icons-react';
 import PropTypes from 'prop-types';
 
 const CSS_NS = 'habits';
 
 class HabitsItemComponent extends Component {
+  /**
+   * Get and return symbol of input day, based on planning and progress
+   *
+   * @param {object} planning
+   * @param {object} progress
+   * @param {moment} day
+   * @return {string}
+   */
   getSymbol(planning, progress, day) {
     const dayOfTheWeek = day.isoWeekday();
+    let materialIcons = '';
+    let materialIconColor = '';
 
     const isPlanned = planning[dayOfTheWeek] === true;
 
     if (!isPlanned) {
-      return '-';
+      // The habit is not planned
+      materialIcons = 'clear';
+      materialIconColor = '#757575'
+    } else if (progress[day.format('YYYY-MM-DD')] === true) {
+      // The habit is planned and done
+      materialIcons = 'check_box';
+      materialIconColor = '#1B5E20';
+      // To use moment comparison, we must ensure the two moment instances have
+      // the same time, so that the are the "same" exactly when the day coincide
+      // Alternative:
+      // if (day.format('YYYY-MM-DD') >= moment().format('YYYY-MM-DD')) {}
+    } else if (day.startOf('day').isSameOrAfter(moment().startOf('day'))) {
+      // The habit is planned, not yet done
+      materialIcons = 'schedule';
+      materialIconColor = '#b38f00';
+    } else {
+      // The habit had been planned in the past, but has not been done
+      materialIcons = 'clear';
+      materialIconColor = '#b71c1c';
     }
 
-    if (progress[day.format('YYYY-MM-DD')] === true) {
-      return 'v';
-    }
-
-    // To use moment comparison, we must ensure the two moment instances have
-    // the same time, so that the are the "same" exactly when the day coincide
-    // Alternative:
-    // if (day.format('YYYY-MM-DD') >= moment().format('YYYY-MM-DD')) {}
-    if (day.startOf('day').isSameOrAfter(moment().startOf('day'))) {
-      return 'fut';
-    }
-
-    return 'pas';
+    return (<MaterialIcon icon={ materialIcons } color={ materialIconColor } size={20} />);
   }
 
   render() {
