7a78c24dd3a2649999f156ef7fa461b26089ce10
LinaLtu <lsuodyte@babbel.com>
2019-01-27 13:20:43 +0100

    fix: added styling to HabitsModal

--- src/pages/HabitsPage.js
+++ src/pages/HabitsPage.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import cx from 'classnames';
 import HabitsHeaderComponent from '../components/HabitsHeaderComponent';
 import HabitsListContaner from '../containers/HabitsListContainer';
 
-const HabitsPage = () => {
+const CSS_NS = 'habits-page';
+
+const HabitsPage = (props) => {
+
+  console.log('From HabitsPage', props.isModalOpen);
+
   return (
-    <div>
+    <div className={cx({[`${CSS_NS}__background-overlay`]: props.isModalOpen})}>
       <HabitsHeaderComponent title={'Habits'} />
       <HabitsListContaner />
     </div>
   );
 };
 
-export default HabitsPage;
+function mapStateToProps(state) {
+  return {
+    isModalOpen: state.isModalOpen
+  }
+}
+
+export default connect(mapStateToProps)(HabitsPage);
